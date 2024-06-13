48ccc9ef8d9d6e2ba483fce520388ac5b5d9f990
umluks <lucas.sga@gmail.com>
2024-06-13 19:35:23 -0300

    mod js

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+let nome;
+let idade;
+let curso;
+
+const nomeCompleto = "Lucas Santiago Gonçalves dos Anjos"
+
+nome = "Lucas Santiago";
+idade = 37;
+curso = "Analise e Desenvolvimento";
+
+
+console.log (nomeCompleto, idade, curso);
